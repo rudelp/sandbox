780ad2c7f9d39813cc01a9b22a3a5ad9e972cb51
Peter Rudel <peter@git.local>
2019-09-13 13:40:36 +0200

    Änderung Resizable

--- src/BrowserTest/Resizeable.java
+++ src/BrowserTest/Resizeable.java
@@ -8,13 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Resizeable {
 	
-	public static WebDriver driver;
-
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C://Users//peru//eclipse-workspace//SeleniumWebDriver//src//resources//chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.google.de");
 		
 	}
 
-}
+
